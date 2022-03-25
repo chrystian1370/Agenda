51141f0449c89e54d37a9c66801010058c7ea63b
Chrystian Alvarenga <99925890+chrystian1370@users.noreply.github.com>
2022-03-25 11:41:58 +0000

    lu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
 contacts = [
-  {'name' : 'Chrystian Alvarenga' },  
+  {'name' : 'Chrystian Alvarenga', 'email' : 'chrystianalvarenga@gmail.com','phone' :'(16)99460-3667'  },  
 ]
 
 @app.route('/')
@@ -12,10 +12,17 @@ def index():
     contacts=contacts
   )
 
+
 @app.route('/create', methods=['POST'])
 def create():
   title = request.form.get('name')
   contacts.append({'name':title})
+  
+  title = request.form.get('email')
+  contacts.append({'email':title})
+  
+  title = request.form.get('phone')
+  contacts.append({'phone':title})
   return redirect ('/')
   
 if __name__ == '__main__':
