ad17ce46bbb7e6e4482552c3782b5d8e53a4a289
Chrystian Alvarenga <99925890+chrystian1370@users.noreply.github.com>
2022-03-25 00:48:03 +0000

    name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
-todos = [
+contact = [
   {'name' : 'Chrystian Alvarenga' },  
 ]
 
@@ -9,13 +9,13 @@ todos = [
 def index():
   return render_template(
     'index.html',
-    todos=todos
+    contact=contact
   )
 
 @app.route('/create', methods=['POST'])
 def create():
   title = request.form.get('name')
-  todos.append({'name':title})
+  contact.append({'name':title})
   return redirect ('/')
   
 if __name__ == '__main__':
